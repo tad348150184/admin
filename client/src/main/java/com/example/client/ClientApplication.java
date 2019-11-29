8625c911e4024768a197491984899834345d4938
谭道霄 <tandx1@lenovo.com>
2019-11-29 15:52:24 +0800

    Initial commit

---
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableEurekaClient
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
